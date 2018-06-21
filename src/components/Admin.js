6bd43381213b6a50380c0107f1be8d6fd655885c
Alexander Rosenthal <alexrosenthalster@gmail.com>
2018-06-21 13:07:57 -0400

    configure cloudinary upload widget

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import ArtForm from './ArtForm';
 
 export default class Admin extends Component {
-  render() {
-      return (
-        <div>Hello World!
-	      <ArtForm />
-	      </div>
+  
+	uploadWidget() {
+		cloudinary.openUploadWidget({
+			cloudname: 'CLOUD_NAME',
+			upload_preset: 'PRESET',
+			tags:[]
+		})
+		.then(result => console.log('result: ', result))
+		.catch(error => console.log('error: ', error))
+	}
+
+	render() {
+          return (
+            <div>Hello World!
+	        <ArtForm />
+		<div className='upload'>
+		  <button onClick={this.uploadWidget.bind(this)} className='upload-btn'>
+		    Add Image
+		  </button>
+		</div>
+	    </div>
       )
   }
 }
