13b3d5674c7ed6b27a28a638b5465f1d653e4aab
Alexander Rosenthal <alexrosenthalster@gmail.com>
2018-06-21 14:40:26 -0400

    write fetch art from cloudinary api actions

---
+++ src/actions/artActions.js
@@ -0,0 +1,40 @@
+import { CLOUDINARY_KEY, CLOUDINARY_SECRET } from '../../config.js';
+
+export const FETCH_ART_BEGIN = 'FETCH_ART_BEGIN';
+export const FETCH_ART_SUCCESS = 'FETCH_ART_SUCCESS';
+export const FETCH_ART_FAIL = 'FETCH_ART_FAIL';
+
+export const fetchArtBegin = () => ({
+	type: FETCH_ART_BEGIN
+});
+
+export const fetchArtSuccess = art => ({
+	type: FETCH_ART_SUCCESS,
+	payload: { art }
+});
+
+export const fetchArtFail = error => ({
+	type: FETCH_ART_FAIL,
+	payload: { error }
+});
+
+export function fetchArt() {
+	return dispatch => {
+		dispatch(fetchArtBegin());
+		return fetch(`https://${CLOUDINARY_KEY}:${CLOUDINARY_SECRET}@api.cloudinary.com/v1_1/prints20/resources/image`)
+			.then(handleErrors)
+			.then(res => res.json())
+			.then(json => {
+				dispatch(fetchArtSuccess(json.art));
+				return json.art;
+			})
+			.catch(error => dispatch(fetchArtFail(error)));
+	};
+}
+
+function handleErrors(response) {
+	if (!response.ok) {
+		throw Error(response.statusText);
+	} 
+	return response;
+}
